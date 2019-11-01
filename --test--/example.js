0b8bb5fdf00a70a00ae8c4e0ef2a84a31cbe2b39
Moses Nwaeze <ogbonnasmart@gmail.com>
2019-11-01 09:25:44 +0100

    testing travis

--- --test--/example.js
+++ --test--/example.js
@@ -1,4 +1,5 @@
 //run with command => jasmine spec/--test--/example.js
+//const api = require('./api/get');
 describe('input forms', ()=>{
 		it('should contain one input', ()=>{
 			expect(forms.count()).toEquals(1);
