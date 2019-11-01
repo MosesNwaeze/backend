b666ce750816c5cc6273800a2d9e88c2c393c734
Moses Nwaeze <ogbonnasmart@gmail.com>
2019-11-01 20:40:28 +0100

    Working on Coveralls

---
+++ spec/test-spec.js
@@ -0,0 +1,27 @@
+var request = require("request");
+
+var base_url = "http://localhost:3000/"
+
+/***  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns Hello World", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("Hello World");
+        done();
+      });
+    });
+  });
+});***/
+describe("Testing value", () => {
+  const num = 0;
+  const num1 = 1;
+  it("Should be false", () =>{
+    expect(num  > num1).toBeFalsy();
+  });
+});
